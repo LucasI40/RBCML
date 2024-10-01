3d9330785d9fa7d9faf6825b6f7b4d9e5f69751b
VictorNetto <vigonetto@gmail.com>
2024-09-30 21:38:21 -0300

    Basic logic of rooms

--- rtc_app/events.py
+++ rtc_app/events.py
@@ -1,4 +1,33 @@
+from collections import namedtuple
+
 from flask import request
 from flask_socketio import emit, join_room
 
 from .extensions import socketio
+
+
+local_database = dict()
+Record = namedtuple('Record', 'user room')
+
+def users_in_room(room):
+    print([local_database[key].user for key in local_database if local_database[key].room == room])
+    return [local_database[key].user for key in local_database if local_database[key].room == room]
+
+@socketio.on('connect')
+def handle_connect():
+    print(f'Client connected - {request.sid}')
+
+@socketio.on('disconnect')
+def handle_close():
+    print(f'Client disconnected - {request.sid}')
+    (user, room) = local_database.pop(request.sid)
+    emit('bye', {'user': user}, to=room)
+
+@socketio.on('join')
+def join(data):
+    user = data['user']
+    room = data['room']
+    local_database[request.sid] = Record(user, room)
+    join_room(room)
+    emit('hello', {'user': user}, to=room, skip_sid=request.sid)
+    emit('update_users_in_room', users_in_room(room), to=room)
--- rtc_app/routes.py
+++ rtc_app/routes.py
@@ -3,6 +3,27 @@ from flask import Blueprint, render_template, redirect, request
 
 main = Blueprint('main', __name__)
 
+
 @main.route('/ping')
-def ping():
+def view_ping():
     return 'pong'
+
+@main.route('/')
+def view_index():
+    return redirect('/login')
+
+@main.route('/login', methods=['GET', 'POST'])
+def view_login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        user = request.form['option']
+        return redirect(f'/user/{user}')
+    
+@main.route('/user/<user>')
+def view_user(user):
+    return render_template('user.html', user=user)
+
+@main.route('/user/<user>/session/<session>')
+def view_session(user, session):
+    return render_template('session.html', user=user, session=session)
