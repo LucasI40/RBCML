40c9e383c12f43d29892696a11a5eabc2cf31cc1
LucasI40 <143128581+LucasI40@users.noreply.github.com>
2025-05-27 22:57:04 -0300

    cratecall

--- rtc_app/static/scripts/home.js
+++ rtc_app/static/scripts/home.js
@@ -1,6 +1,22 @@
+let template1 = {
+    "id" : 1,
+    "name" : "Sala de Aula",
+    "image" : "sala-aula.jpg"
+};
+let template2 = {
+    "id" : 2,
+    "name" : "Corte",
+    "image" : "corte.jpg"
+};
+let template3 = {
+    "id" : 3,
+    "name" : "Auditorio",
+    "image" : "auditorio.jpg"
+};
+
+
 function changeLayout(layout){
-    const layoutElement = document.querySelector('.layouts');
-    layoutElement.src = layout;
+    document.querySelector('.teste').innerHTML = layout;
 }
 
 
